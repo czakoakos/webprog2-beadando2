0764b18b35f25deff8f28be74f05423072192ea9
czakoakos <117032204+czakoakos@users.noreply.github.com>
2022-11-30 23:56:58 +0100

    köszöntő oldal styling

--- views/logger.php
+++ views/logger.php
@@ -1,1 +1,10 @@
-<h2><br><?= $viewData['uzenet']?><br></h2>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 col-lg-6 text-center py-4">
+            <h1 class="red-text mb-3">Kedves <?= $viewData['uzenet']?>!</h1>
+            <p class="blue-text mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vulputate sem ut nunc
+                dictum, non commodo sem bibendum. </p>
+            <p><strong>További jó munkát!</strong></p>
+        </div>
+    </div>
+</div>
