47fd89058a8ed75e55842e2e56af0081aa400160
BroKoda <95308317+BroKoda@users.noreply.github.com>
2022-12-01 13:10:27 +0100

    jQuery added

--- views/page_template.php
+++ views/page_template.php
@@ -62,5 +62,13 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.1.slim.min.js"
+        integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script>
+
+<!--<script>-->
+<!--    $( document ).ready(function() {-->
+<!--        console.log( "Page loaded successfully with jQuery!" );-->
+<!--    });-->
+<!--</script>-->
 </body>
 </html>
